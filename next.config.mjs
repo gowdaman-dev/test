88bb711ee377c56babe60339070a8692c54ce3ce
gowdaman-dev <damangowdaman@gmail.com>
2024-02-09 03:52:23 -0600

    new

--- next.config.mjs
+++ next.config.mjs
@@ -1,31 +1,4 @@
-const getCorsHeaders = () => {
-    const headers = {};
-    
-    headers["Access-Control-Allow-Origin"] = "http://localhost:3000";
-    headers["Access-Control-Allow-Credentials"] = "true";
-    headers["Access-Control-Allow-Methods"] =
-      "GET,OPTIONS,PATCH,DELETE,POST,PUT";
-    headers["Access-Control-Allow-Headers"] =
-      "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization";
-  
-    return Object.entries(headers).map(([key, value]) => ({ key, value }));
-  };
-  
-  const nextConfig = {
-    reactStrictMode: true,
-    experimental: {
-      serverActions: true,
-    },
-    headers: async () => {
-      return [
-        {
-          source: "/api/(.*)",
-          headers: getCorsHeaders(),
-        },
-      ];
-    },
-    swcMinify: true,
-  };
-  
-  module.exports = withBundleAnalyzer(nextConfig);
-  
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+export default nextConfig;
